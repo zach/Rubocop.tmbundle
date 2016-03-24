f1162c8848dbb20e8e36a93373bf223b01dd65a9
Zach Baker <zach@zachbaker.com>
2016-03-23 17:36:20 -0700

    Update to support Rubocop 0.36.0 and later

--- Support/rubocop.rb
+++ Support/rubocop.rb
@@ -14,7 +14,7 @@ def log(msg)
 end
 
 def offences(file)
-  processed_source = RuboCop::ProcessedSource.from_file(file)
+  processed_source = RuboCop::ProcessedSource.from_file(file, RUBY_VERSION.to_f)
   team = RuboCop::Cop::Team.new(
     RuboCop::Cop::Cop.all,
     RuboCop::ConfigStore.new.for(processed_source.path),
